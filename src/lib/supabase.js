97405a3574aa2a477ab216b565f4a051be9e06b6
pencilwasinsidebox <kalraaryan67@gmail.com>
2026-04-14 11:01:29 +0530

    refactor: move supabase configuration to environment variables

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = 'https://ltydxeuqiwqxmvhjinhr.supabase.co'
-const supabaseAnonKey =
-  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx0eWR4ZXVxaXdxeG12aGppbmhyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzM0MTQ0NzgsImV4cCI6MjA4ODk5MDQ3OH0._0JQJc5MzhtvIzArdMpe3SLvZ9lJfAug4dOjSmQ2-8U'
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseAnonKey = import.meta.env.VITE_SUPABASE_ANON_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey)
